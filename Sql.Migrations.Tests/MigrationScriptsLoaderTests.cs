1031ed67e5fc4a8ea51c77f77490a73868105185
Christian Folie <christian.folie@outlook.com>
2016-08-10 11:58:11 +0200

    added fallback for directory lookup

--- Sql.Migrations.Tests/MigrationScriptsLoaderTests.cs
+++ Sql.Migrations.Tests/MigrationScriptsLoaderTests.cs
@@ -5,7 +5,16 @@ namespace Sql.Migrations.Tests
 {
     public class MigrationScriptsLoaderTests
     {
-        static readonly string ResourceDirectory = Path.Combine(Directory.GetCurrentDirectory(), "resources");
+        static readonly string ResourceDirectory;
+        static MigrationScriptsLoaderTests()
+        {
+            const string resource = "resources";
+            var dir = Path.Combine(Directory.GetCurrentDirectory(), resource);
+            if (!Directory.Exists(dir))
+                dir = Path.Combine(Directory.GetCurrentDirectory(), "Sql.Migrations.Tests", resource);
+
+            ResourceDirectory = dir;
+        }
 
         [Fact]
         public void Searching_with_non_existing_extension_should_lead_to_no_result()
@@ -17,10 +26,11 @@ namespace Sql.Migrations.Tests
         public void Searching_with_the_correct_suffix_should_lead_to_two_entries()
         {
             Assert.Collection(MigrationScriptsLoader.FromDirectory(ResourceDirectory, "*.sql"),
-                x => {
+                x =>
+                {
                     Assert.Equal("test1.sql", x.Name);
                     Assert.Equal("test1", x.Script);
-                    },
+                },
                 x =>
                 {
                     Assert.Equal("test2.sql", x.Name);
